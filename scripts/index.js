a148164aa50d38a96c964a7c200b2d9165dd07fe
Olga Dorofeeva <olga.dorof3eva@yandex.ru>
2024-06-12 20:16:21 +0300

    first init

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: Функция создания карточки
+initialCards.forEach(function (element) {
+  const cardName = element.name;
+  const cardImg = element.link;
+  placesList.append(cardsAdd(cardName, cardImg, cardDelete));
+});
 
-// @todo: Функция удаления карточки
+// Функция создания карточки
 
-// @todo: Вывести карточки на страницу
+function cardsAdd(cardName, cardImg, cardDelete) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = cardName;
+  cardElement.querySelector(".card__image").src = cardImg;
+  cardElement.querySelector(".card__image").alt = cardName;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", cardDelete);
+  return cardElement;
+}
+
+// Функция удаления карточки
+
+function cardDelete(evt) {
+  const eventTarget = evt.target;
+  eventTarget.closest(".card").remove();
+}
+
